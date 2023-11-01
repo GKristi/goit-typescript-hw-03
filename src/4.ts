22dfcc7f972dae09231ff2a96d9570635937d3ee
Kristina Holub <christigolub@gmail.com>
2023-11-01 08:20:48 +0200

    completed tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,41 @@
+class Key {
+    private signature: number = Math.random();
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+        this.key = key;
+    }
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+    abstract openDoor(key: Key): void;
+    comeIn(tenant: Person): void {
+        if (this.door) {
+            this.tenants.push(tenant);
+        }
+    }
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +46,4 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
